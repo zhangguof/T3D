fc06b5d65564c381f88e77545255ea28f5ea90cd
tony <zhangguof@gmail.com>
2016-12-11 10:48:22 +0800

    windows support.

--- src/T3D.hpp
+++ src/T3D.hpp
@@ -5,10 +5,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <stdlib.h>
-#include <stdio.h>
+#ifdef _WIN32
+#include <direct.h>
+#include <io.h>
+#endif
 
+#ifdef __APPLE__
 #include <unistd.h>
+#endif
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
